08ee35178e1eccbb07077a81542dccb98816b790
edulon2000 <edulon2015@gmail.com>
2024-06-03 15:29:34 -0300

    Executable upload

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ mode_label = Label(text=" ", fg = GREEN,bg=YELLOW, font=(FONT_NAME,26,"bold"))
 mode_label.grid(column=1,row=1)
 
 canvas = Canvas(width=200, height=224,bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="pomodoro-start\\tomato.png")
+tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100,112, image = tomato_img)
 timer_text = canvas.create_text(100,130, text="00:00",fill= "white", font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1,row=2)
